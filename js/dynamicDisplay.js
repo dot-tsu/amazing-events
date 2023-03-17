84fbe9d70049d8c5797400001bb4668493595c44
Tsu <lucanahtsu@gmail.com>
2023-03-17 11:51:31 -0300

    Extract of getData() function to separate file

--- js/dynamicDisplay.js
+++ js/dynamicDisplay.js
@@ -1,20 +1,3 @@
-/* API */
-async function getData() {
-    const localDataPath = "../json/data.json"
-    try {
-        const response = await fetch("https://mindhub-xj03.onrender.com/api/amazing");
-        const data = await response.json();
-        console.log("Data successfully retrieved from the API. 🐱");
-        return data;
-    } catch (error) {
-        console.error(error, "\nFailed to retrieve data from the API, using local data. 😿");
-        const localResponse = await fetch(localDataPath);
-        const localData = await localResponse.json();
-        console.log("Data successfully retrieved from local JSON. 🐱");
-        return localData;
-    }
-}
-
 /* Display Content */
 async function main() {
 
