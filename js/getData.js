aa2ff0dd26159170172a2796a13ca8bc2116021b
Tsu <lucanahtsu@gmail.com>
2023-03-22 11:51:34 -0300

    add a console.info when its retrieving data

--- js/getData.js
+++ js/getData.js
@@ -1,16 +1,22 @@
 /* API Data retriever*/
 async function getData() {
-    const localDataPath = "../json/data.json"
-    try {
-        const response = await fetch("https://mindhub-xj03.onrender.com/api/amazing");
-        const data = await response.json();
-        console.info("Data successfully retrieved from the API. 🐱");
-        return data;
-    } catch (error) {
-        console.error(error, "\nFailed to retrieve data from the API, using local data. 😿");
-        const localResponse = await fetch(localDataPath);
-        const localData = await localResponse.json();
-        console.info("Data successfully retrieved from local JSON. 🐱");
-        return localData;
-    }
+  const localDataPath = "../json/data.json";
+  console.info("Retrieving data... 😸");
+  try {
+    const response = await fetch(
+      "https://mindhub-xj03.onrender.com/api/amazing"
+    );
+    const data = await response.json();
+    console.info("Data successfully retrieved from the API. 🐱");
+    return data;
+  } catch (error) {
+    console.error(
+      error,
+      "\nFailed to retrieve data from the API, using local data. 😿"
+    );
+    const localResponse = await fetch(localDataPath);
+    const localData = await localResponse.json();
+    console.info("Data successfully retrieved from local JSON. 🐱");
+    return localData;
+  }
 }
